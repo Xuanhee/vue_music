e2ec13124e31b62e741afcce860c2f4414a1c5cd
Xuanhee <529177573@qq.com>
2020-06-21 22:00:56 +0800

    排行不显示修复

--- src/api/config.js
+++ src/api/config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: your name
+ * @Date: 2020-06-21 17:32:28
+ * @LastEditTime: 2020-06-21 21:51:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \vue_music\src\api\config.js
+ */ 
 // 配置跨域请求时通用的参数
 // 按需导出
 export const commonParams = {
@@ -5,7 +13,7 @@ export const commonParams = {
   inCharset: 'utf-8',
   outCharset: 'utf-8',
   notice: 0,
-  format: 'json'
+  format: 'jsonp'
 }
 export const options = {
   param: 'jsonpCallback',
